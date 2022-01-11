40944d02c2a5d6ea1c498cd6c3fae17425863c69
Tejas <tejasram03@gmail.com>
2022-01-11 23:02:47 +0530

    created a function to display the board resolves #1

---
+++ Main.py
@@ -0,0 +1,8 @@
+#TicTacToe program
+
+def display_board(board):
+    print(board[1],"||",board[2],"||",board[3])
+    print("-"*11)
+    print(board[4],"||",board[5],"||",board[6])
+    print("-"*11)
+    print(board[7],"||",board[8],"||",board[9])
